61cf53bc9aac9d8fb7e04f941274d084d2f037a9
Abd-ul-Ahad <abd-ul-ahad@outlook.com>
2022-04-30 07:15:13 +0500

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-nf'
+import 'react-nf/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
